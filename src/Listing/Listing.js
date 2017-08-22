ea40e1c2f9ab81533d4907be982e983e6fdfa5f8
Emilia Jendrusik <emilia.jendrusik@gmail.com>
2017-08-22 21:11:13 +0200

    Partial filters part 1

--- src/Listing/Listing.js
+++ src/Listing/Listing.js
@@ -4,17 +4,36 @@ import "./Listing.css";
 class Listing extends Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			updatedFilter : "petType"
+		}
 	}
+	shouldComponentUpdate(nextProps, nextState) {
+    	if (this.props.petType !== nextProps.petType) {
+    		this.setState({updatedFilter: "petType"});
+	      	return true;
+	    }
+	    if (this.props.petSize !== nextProps.petSize) {
+	    	this.setState({updatedFilter: "petSize"});
+	      return true;
+	    } 
+	    if (this.props.petAge !== nextProps.petAge) {
+	    	this.setState({updatedFilter: "petAge"});
+	      return true;
+	    } 
+    	return false;
+  	}
 	render() {
-		console.log(this.props.petType);
 		//Filter by Type
-		let filterType = this.props.petType;
+		console.log("rerender");
+		let filterName = this.state.updatedFilter;
+		let filterType = this.props[filterName];
 		let filteredPets;
 		if (filterType == "none") {
 			filteredPets = this.props.petsData;
 		} else {
 			filteredPets = this.props.petsData.filter(function(pet) {
-		    	return pet.type == filterType;
+		    	return pet[filterName] == filterType;
 		 	}).map(function(pet) { return pet });
 		}
 		let mappedPets  = filteredPets.map(function(pet) {
