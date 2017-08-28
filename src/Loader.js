81b2faa60e0756ac8327ea27bcf46f3dc9b16324
Emilia Jendrusik <emilia.jendrusik@gmail.com>
2017-08-28 12:46:38 +0200

    Refactor loader

--- src/Loader.js
+++ src/Loader.js
@@ -1,11 +1,7 @@
-import React, {Component} from 'react';
+import React from 'react';
 import loader from './images/loader.gif';
 
-class Loader extends Component  {
-	render() {
-		return (
-			<div className="loader"><img src={loader} /></div>
-		)
-	}
+const Loader = () =>  {
+	return <div className="loader"><img src={loader} /></div>
 }
 export default Loader;
